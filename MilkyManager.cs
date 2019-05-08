49dc329bc3298ce9c2de896ec37377bea2570e97
Matt <33204743+Laiteux@users.noreply.github.com>
2019-05-08 14:35:14 +1100

    Update MilkyManager.cs

--- MilkyManager.cs
+++ MilkyManager.cs
@@ -1,3 +1,4 @@
+using Milky.Core;
 using Milky.Loops;
 using Milky.Output;
 using Milky.Program;
@@ -8,63 +9,71 @@ using Milky.Utils;
 
 namespace LoL_Checker
 {
-    class MilkyManager
-    {
-        public ConsoleLoops ConsoleLoops;
-        public LoopsManager LoopsManager;
-        public StatisticsLoops StatisticsLoops;
-
-        public OutputSettings OutputSettings;
-
-        public ProgramInformations ProgramInformations;
-        public ProgramManager ProgramManager;
-
-        public RunInformations RunInformations;
-        public RunLists RunLists;
-        public RunManager RunManager;
-
-        public ConsoleSettings ConsoleSettings;
-        public RunSettings RunSettings;
-
-        public CustomStatistics CustomStatistics;
-        public RunStatistics RunStatistics;
-
-        public ConsoleUtils ConsoleUtils;
-        public DateTimeUtils DateTimeUtils;
-        public FileUtils FileUtils;
-        public FormatUtils FormatUtils;
-        public ListUtils ListUtils;
-        public RequestUtils RequestUtils;
-        public UserUtils UserUtils;
-
-        public MilkyManager()
-        {
-            ConsoleLoops = ConsoleLoops.GetOrNewInstance();
-            LoopsManager = LoopsManager.GetOrNewInstance();
-            StatisticsLoops = StatisticsLoops.GetOrNewInstance();
-
-            OutputSettings = OutputSettings.GetOrNewInstance();
-
-            ProgramInformations = ProgramInformations.GetOrNewInstance();
-            ProgramManager = ProgramManager.GetOrNewInstance();
-
-            RunInformations = RunInformations.GetOrNewInstance();
-            RunLists = RunLists.GetOrNewInstance();
-            RunManager = RunManager.GetOrNewInstance();
-
-            ConsoleSettings = ConsoleSettings.GetOrNewInstance();
-            RunSettings = RunSettings.GetOrNewInstance();
-
-            CustomStatistics = CustomStatistics.GetOrNewInstance();
-            RunStatistics = RunStatistics.GetOrNewInstance();
-
-            ConsoleUtils = ConsoleUtils.GetOrNewInstance();
-            DateTimeUtils = DateTimeUtils.GetOrNewInstance();
-            FileUtils = FileUtils.GetOrNewInstance();
-            FormatUtils = FormatUtils.GetOrNewInstance();
-            ListUtils = ListUtils.GetOrNewInstance();
-            RequestUtils = RequestUtils.GetOrNewInstance();
-            UserUtils = UserUtils.GetOrNewInstance();
-        }
-    }
+	class MilkyManager
+	{
+		public Authentication Authentication;
+		
+		public ConsoleLoops ConsoleLoops;
+		public LoopsManager LoopsManager;
+		public StatisticsLoops StatisticsLoops;
+		
+		public OutputSettings OutputSettings;
+		
+		public ProgramInformations ProgramInformations;
+		public ProgramManager ProgramManager;
+		
+		public RunInformations RunInformations;
+		public RunLists RunLists;
+		public RunManager RunManager;
+		
+		public ConsoleSettings ConsoleSettings;
+		public RunSettings RunSettings;
+		
+		public CustomStatistics CustomStatistics;
+		public RunStatistics RunStatistics;
+		
+		public ConsoleUtils ConsoleUtils;
+		public DateTimeUtils DateTimeUtils;
+		public FileUtils FileUtils;
+		public FormatUtils FormatUtils;
+		public HashUtils HashUtils;
+		public ListUtils ListUtils;
+		public RequestUtils RequestUtils;
+		public StringUtils StringUtils;
+		public UserUtils UserUtils;
+		
+		public MilkyManager()
+		{
+			Authentication = Authentication.GetOrNewInstance();
+			
+			ConsoleLoops = ConsoleLoops.GetOrNewInstance();
+			LoopsManager = LoopsManager.GetOrNewInstance();
+			StatisticsLoops = StatisticsLoops.GetOrNewInstance();
+			
+			OutputSettings = OutputSettings.GetOrNewInstance();
+			
+			ProgramInformations = ProgramInformations.GetOrNewInstance();
+			ProgramManager = ProgramManager.GetOrNewInstance();
+			
+			RunInformations = RunInformations.GetOrNewInstance();
+			RunLists = RunLists.GetOrNewInstance();
+			RunManager = RunManager.GetOrNewInstance();
+			
+			ConsoleSettings = ConsoleSettings.GetOrNewInstance();
+			RunSettings = RunSettings.GetOrNewInstance();
+			
+			CustomStatistics = CustomStatistics.GetOrNewInstance();
+			RunStatistics = RunStatistics.GetOrNewInstance();
+			
+			ConsoleUtils = ConsoleUtils.GetOrNewInstance();
+			DateTimeUtils = DateTimeUtils.GetOrNewInstance();
+			FileUtils = FileUtils.GetOrNewInstance();
+			FormatUtils = FormatUtils.GetOrNewInstance();
+			HashUtils = HashUtils.GetOrNewInstance();
+			ListUtils = ListUtils.GetOrNewInstance();
+			RequestUtils = RequestUtils.GetOrNewInstance();
+			StringUtils = StringUtils.GetOrNewInstance();
+			UserUtils = UserUtils.GetOrNewInstance();
+		}
+	}
 }
