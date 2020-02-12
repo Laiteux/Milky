069a8a51eb2c263be61504a72219778005c8e4f7
Matt <33204743+Laiteux@users.noreply.github.com>
2020-02-13 00:50:10 +1100

    Added "using System.Linq" on examples

--- examples/Club_Cooee_Checker.cs
+++ examples/Club_Cooee_Checker.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
--- examples/Spotify_Checker.cs
+++ examples/Spotify_Checker.cs
@@ -3,6 +3,7 @@ using Milky.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
